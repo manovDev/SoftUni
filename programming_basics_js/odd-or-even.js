4a5f0ade682739cfedbc7d13aa57255a503f348a
Ivo Manov <ivoivchov@gmail.com>
2020-12-10 01:07:16 +0200

    Delete odd-or-even.js

--- programming_basics_js/odd-or-even.js
+++
@@ -1,9 +0,0 @@
-function oddOrEven(input) {
-    let num = Number(input.shift());
-    if (num % 2 === 0) {
-        console.log("Even");
-    }else {
-        console.log("Odd");
-    }
-}
-oddOrEven([4]);
