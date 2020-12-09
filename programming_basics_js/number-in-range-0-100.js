fdbca335783f2483e5ccb3d3409f7da67d040bc8
Ivo Manov <ivoivchov@gmail.com>
2020-12-10 01:06:57 +0200

    Delete number-in-range-0-100.js

--- programming_basics_js/number-in-range-0-100.js
+++
@@ -1,9 +0,0 @@
-function solve(input) {
-    let n = Number(input.shift());
-    while(n < 1 || n > 100) {
-        console.log("Invalid number!");
-        n = Number(input.shift());
-    }
-    console.log("The number is: " + n);
-}
-solve([105]);
