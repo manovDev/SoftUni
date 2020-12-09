3cab4ba2b8f6c35ad9fcea870f8ac79da6873855
Ivo Manov <ivoivchov@gmail.com>
2020-12-10 01:03:06 +0200

    Delete circle-area-peremeter.js

--- programming_basics_js/circle-area-peremeter.js
+++
@@ -1,8 +0,0 @@
-function areaAndPeremeterOfCircle(input) {
-    let r = Number(input.shift());
-    let area = Math.PI * r * r;
-    let p = 2 * Math.PI * r;
-    console.log(area.toFixed(2));
-    console.log(p.toFixed(2));
-}
-areaAndPeremeterOfCircle(["3"]);
