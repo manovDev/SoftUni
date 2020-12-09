a15e829d3655c5c9bbdcf151398ddac56713376b
Ivo Manov <ivoivchov@gmail.com>
2020-12-10 01:05:47 +0200

    Delete inces-to-cantemeters.js

--- programming_basics_js/inces-to-cantemeters.js
+++
@@ -1,6 +0,0 @@
-function inchesToCentemeters(input) {
-    let inches = Number(input.shift());
-    let convertToCentemeters = inches * 2.54;
-    console.log(convertToCentemeters.toFixed(2));
-}
-inchesToCentemeters(["5"]);
